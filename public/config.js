04c0aa7b88a45176f42d9e6c48d2072a55a78044
Mario Reder <mario.reder@pm.me>
2026-02-27 19:16:01 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -24,7 +24,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
-  "VITE_TWITTER_URL": "",
+  "VITE_TWITTER_URL": "https://x.com/PangeonPrime",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
   "VITE_SEO_SITE_URL": "https://dex.orderly.network/sygnai-5086/",
